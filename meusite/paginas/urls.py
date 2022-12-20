a8ab891861b83ddd29f49162c8547dcec44b0fd5
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2022-12-20 10:32:18 -0300

    finalização de projeto

--- meusite/paginas/urls.py
+++ meusite/paginas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index'),
-    path('sobre/', views.index, name='sobre')
+    path('', views.index, name='pagina_inicial'),
+    path('sobre/', views.sobre, name='sobre')
 ]
